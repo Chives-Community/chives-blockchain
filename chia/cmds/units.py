5af315e87c864a1b5b5c2d318e99db581df8c244
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2022-04-27 10:50:58 -0500

    Update units.py
    match Chives units

--- chia/cmds/units.py
+++ chia/cmds/units.py
@@ -3,7 +3,7 @@ from typing import Dict
 # The rest of the codebase uses mojos everywhere.
 # Only use these units for user facing interfaces.
 units: Dict[str, int] = {
-    "chia": 10 ** 12,  # 1 chia (XCH) is 1,000,000,000,000 mojo (1 trillion)
+    "chia": 10 ** 8,  # 1 chives (XCC) is 100,000,000 mojo (100 million)
     "mojo": 1,
-    "cat": 10 ** 3,  # 1 CAT is 1000 CAT mojos
+    "cat": 10 ** 5,  # 1 cat coin is 100,000 CAT mojos
 }
